d30527fd2bd7375658b4fff797a1d696d03893e6
Katie G <oharagolden@gmail.com>
2018-09-16 20:56:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function inc(n) {
 function dec(n) {
   return (n-= 1)
 }
-function makeInt(n) {
-  parseInt('n', 10)
+function makeInt(n, base) {
+  var parsed = parseInt('n', 10)
   return (n)
 }
 function preserveDecimal(n) {
